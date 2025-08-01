afa3fa52db2b9a75f6be485bc81bf8da63d14aef
TheLegendsGaming <113374148+TheLegendsGaming@users.noreply.github.com>
2025-08-01 10:56:32 +0300

    Add files via upload

---
+++ tracer.py
@@ -0,0 +1,17 @@
+import phonenumbers
+from phonenumbers import geocoder
+
+import time, random
+
+def start_phone_tracer(target):
+    print(f"[+] PhoneTracer v3.1")
+    print(f"[+] Target: {target}")
+    print(f"[+] Tracing..")
+    time.sleep(2)
+    p = phonenumbers.parse(target)
+    r = geocoder.description_for_number(p,"en")
+    print(f"[+] location: {r}")
+    print(f"[+] Trace complete")
+start_phone_tracer(input("Target Phone Number: "))
+    
+    
